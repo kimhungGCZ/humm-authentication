79d33d47a710726948f8042eb939f369bb8556c7
Kim Hung LE <kim.hung@greencityzen.fr>
2017-11-20 16:49:50 +0100

    update env - 1

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
   AUTH0_CALLBACK_URL:
-    process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+    process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/user'
 };
 
 /* GET home page. */
@@ -48,5 +48,6 @@ router.get('/failure', function(req, res) {
     error_description: error_description[0],
   });
 });
+
 
 module.exports = router;
